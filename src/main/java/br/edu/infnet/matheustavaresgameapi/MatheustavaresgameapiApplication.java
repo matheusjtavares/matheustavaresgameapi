f5a2dab6c0fe13b00ce354910e5c1d93f42246dc
Matheus Tavares <matheusce55@gmail.com>
2025-09-28 23:53:53 -0300

    Initialize new repository

---
+++ src/main/java/br/edu/infnet/matheustavaresgameapi/MatheustavaresgameapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.matheustavaresgameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheustavaresgameapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheustavaresgameapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/matheustavaresgameapi/MatheustavaresgameapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.matheustavaresgameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheustavaresgameapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
