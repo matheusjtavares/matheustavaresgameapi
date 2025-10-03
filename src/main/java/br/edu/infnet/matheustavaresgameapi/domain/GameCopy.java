19ff19e13f1deed848d8fb73f201aa07fd4d6bf9
Matheus Tavares <matheusce55@gmail.com>
2025-10-03 11:17:05 -0300

    fix: remove imports

--- src/main/java/br/edu/infnet/matheustavaresgameapi/domain/GameCopy.java
+++ src/main/java/br/edu/infnet/matheustavaresgameapi/domain/GameCopy.java
@@ -1,19 +1,5 @@
-package br.edu.infnet.matheustavaresapi.model.domain;
 
 import java.time.LocalDate;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 
 public class GameCopy {
     private Integer id;
@@ -21,7 +7,7 @@ public class GameCopy {
     private LocalDate ownedSinceDate;
     private String player;
     private boolean isForRental;
-    
+
     public Integer getId() {
         return id;
     }
