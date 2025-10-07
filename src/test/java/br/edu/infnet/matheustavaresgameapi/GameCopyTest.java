a5198e07bcde04fc1fb489a2ef4ffa7680a80b77
Matheus Tavares <matheusce55@gmail.com>
2025-10-06 23:32:46 -0300

    feature: add missing displaynames

--- src/test/java/br/edu/infnet/matheustavaresgameapi/GameCopyTest.java
+++ src/test/java/br/edu/infnet/matheustavaresgameapi/GameCopyTest.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import br.edu.infnet.matheustavaresgameapi.model.domain.GameCopy;
@@ -16,12 +17,14 @@ public class GameCopyTest {
     }
     
     @Test
+    @DisplayName("Must throw GameCopyInvalidException when ID is less than 1")
     public void mustThrowGameCopyInvalidException_whenIdIsLessThanOne() {
         assertThrows(GameCopyInvalidException.class,() -> new GameCopy(0, "Final Fantasy",LocalDate.now(),"Matheus", new BigDecimal(10), true,10));
 
     }
     
     @Test
+    @DisplayName("Must throw GameCopyInvalidException ownedSinceDate is in the future")
     public void mustThrowGameCopyInvalidException_whenOwnedDateIsInTheFuture() {
         assertThrows(GameCopyInvalidException.class,() -> new GameCopy(1, "Final Fantasy",LocalDate.ofYearDay(2026, 1),"Matheus", new BigDecimal(10), true,10));
 
