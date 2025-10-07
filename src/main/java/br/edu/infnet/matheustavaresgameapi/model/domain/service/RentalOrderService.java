c335e00571b8d8f93a17e69f76292d69f6cf9eba
Matheus Tavares <matheusce55@gmail.com>
2025-10-06 22:07:37 -0300

    feature: create service class for rental orders

---
+++ src/main/java/br/edu/infnet/matheustavaresgameapi/model/domain/service/RentalOrderService.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.matheustavaresgameapi.model.domain.service;
+
+import br.edu.infnet.matheustavaresgameapi.model.domain.RentalOrder;
+
+public class RentalOrderService {
+    public boolean persistRentalOrder(RentalOrder rentalOrder){
+        throw new UnsupportedOperationException("Method not implemented");
+    }
+}
