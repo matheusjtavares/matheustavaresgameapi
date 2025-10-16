8c04d365a9c49a7eb578037cc0f6598753754e59
Matheus Tavares <matheusce55@gmail.com>
2025-10-15 22:59:54 -0300

    first commit

---
+++ main-app/src/test/java/br/edu/infnet/main_app/MainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.main_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/br/edu/infnet/games_parent/GamesParentApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.games_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesParentApplication.class, args);
+	}
+
+}
