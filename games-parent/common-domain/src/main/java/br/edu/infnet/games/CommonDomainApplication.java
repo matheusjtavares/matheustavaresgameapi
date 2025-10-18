5ddf4dfd15b6f84694baa607380e9a2742e72917
Matheus Tavares <matheusce55@gmail.com>
2025-10-18 02:42:36 -0300

    move files

---
+++ games-parent/common-domain/src/main/java/br/edu/infnet/games/CommonDomainApplication.java
@@ -0,0 +1,8 @@
+package br.edu.infnet.games;
+
+public class CommonDomainApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ games-parent/external-api/src/main/java/br/edu/infnet/games/ExternalApiApplication.java
@@ -0,0 +1,8 @@
+package br.edu.infnet.games;
+
+public class ExternalApiApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
